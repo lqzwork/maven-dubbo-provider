5ee41d12f818ff52be1493adef1b5939a381596d
李清正 <liqz@lifengyun.com>
2017-07-14 18:29:13 +0800

    多个provider注册服务到不同的zk（一个是81一个是82），consumer同时订阅者两个服务中心（zk）的服务，既：同时使用两个服务中的的service

---
+++ src/main/java/com/liqz/modules/dubbo/service/MyService2Imp.java
@@ -0,0 +1,9 @@
+package com.liqz.modules.dubbo.service;
+
+import com.liqz.modules.dubbo.api.MyService2;
+
+public class MyService2Imp implements MyService2 {
+    public String getName2() {
+        return "success2";
+    }
+}
---
+++ src/main/java/com/liqz/modules/dubbo/start/StartDubboProvider2.java
@@ -0,0 +1,13 @@
+package com.liqz.modules.dubbo.start;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StartDubboProvider2 {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[] {"spring-context-dubbo-provider2.xml" });
+        context.start();
+        System.out.println("---服务启动成功！！！---");
+        System.in.read(); // 为保证服务一直开着，利用输入流的阻塞来模拟
+    }
+}
